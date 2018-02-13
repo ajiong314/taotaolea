be6e009e59f59648f1efd61a6f02b5d42f0fd8cd
shenyuming <845389937@qq.com>
2018-02-14 00:22:56 +0800

    处理post请求逻辑判断’

--- dailyfresh_24/apps/users/views.py
+++ dailyfresh_24/apps/users/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic import View
+from django.core.urlresolvers import reverse
+import re
 # Create your views here.
 
 
@@ -11,6 +13,25 @@ class RegisterView(View):
         return render(request, 'register.html')
 
     def post(self,request):
+
+        name = request.POST.get('user_name')
+        pwd = request.POST.get('pwd')
+        email = request.POST.get('email')
+        allow = request.POST.get('allow')
+
+        if all([name, pwd, email, allow]):
+
+            return redirect(reversed('users:register'))
+
+        if re.match(r'/^[a-z0-9][\w\.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$/', email):
+
+            return render(request, 'register.html', {'errmsg': '邮箱格式错误'})
+
+        if allow != 'on':
+
+            return render(request, 'register.html', {'errmsg': '请勾选用户协议'})
+
+
 
         return HttpResponse('ceshi')
 
